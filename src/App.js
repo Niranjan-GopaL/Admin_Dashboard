5413abc97abac8cfe3c8316bfbf0425f9cd62017
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-03-27 03:57:38 +0530

    Added starter files , will create files and folder structure next

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './App.css'
+
+const App = () => {
+  return (
+    <h1 className='underline text-3xl'>
+      App
+    </h1>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+
+import App from './App';
+import './index.css'
+
+ReactDOM.render(<App />,document.getElementById('root'));
+
+// the only file that imports React and ReactDOM.
+// the only lines that remain same throughout most of React projects
